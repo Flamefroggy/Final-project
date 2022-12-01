45259aa0b36fd153ecb4ee7bddf1ad6d4c5c7109
FlameFrog <revec1251@gmail.com>
2022-12-02 02:19:42 +0300

    method for create an array

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,4 +1,23 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
+string[] CreateArray(int number)
+{
+    string [] startArray = new string [number];
+    for (int i = 0; i<number; i++)
+    {
+        if (i%10 == 2 && i % 100 != 12)
+        {
+            Console.Write($"Введите {i+1}-ье слово: "); 
+        }
+        else
+        {
+            Console.Write($"Введите {i+1}-ое слово: ");
+        }
+        startArray[i] = Console.ReadLine();
+            
+    }
+    return startArray;
+}
+
 Console.Write("Введите натуральное число, обозначающее количество слов, которые Вы введёте: ");
 int n = Convert.ToInt32(Console.ReadLine());
